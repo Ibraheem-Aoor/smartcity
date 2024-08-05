127e7059f6c7742c5cd85746d594504c5bafbda4
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-08-05 22:52:38 +0300

    init

---
+++ resources/views/layouts/site/master.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Travela - Tourism Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('assets/site/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/site/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('assets/site/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('assets/site/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary px-5 d-none d-lg-block">
+        <div class="row gx-0">
+            <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
+                <div class="d-inline-flex align-items-center" style="height: 45px;">
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
+                            class="fab fa-twitter fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
+                            class="fab fa-facebook-f fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
+                            class="fab fa-linkedin-in fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
+                            class="fab fa-instagram fw-normal"></i></a>
+                    <a class="btn btn-sm btn-outline-light btn-sm-square" href=""><i
+                            class="fab fa-youtube fw-normal"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 text-center text-lg-end">
+                <div class="d-inline-flex align-items-center" style="height: 45px;">
+                    <a href="#"><small class="me-3 text-light"><i class="fa fa-user me-2"></i>Register</small></a>
+                    <a href="#"><small class="me-3 text-light"><i
+                                class="fa fa-sign-in-alt me-2"></i>Login</small></a>
+                    <div class="dropdown">
+                        <a href="#" class="dropdown-toggle text-light" data-bs-toggle="dropdown"><small><i
+                                    class="fa fa-home me-2"></i> My Dashboard</small></a>
+                        <div class="dropdown-menu rounded">
+                            <a href="#" class="dropdown-item"><i class="fas fa-user-alt me-2"></i> My Profile</a>
+                            <a href="#" class="dropdown-item"><i class="fas fa-comment-alt me-2"></i> Inbox</a>
+                            <a href="#" class="dropdown-item"><i class="fas fa-bell me-2"></i> Notifications</a>
+                            <a href="#" class="dropdown-item"><i class="fas fa-cog me-2"></i> Account Settings</a>
+                            <a href="#" class="dropdown-item"><i class="fas fa-power-off me-2"></i> Log Out</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+    {{-- NAV START --}}
+    <!-- Navbar & Hero Start -->
+    <div class="container-fluid position-relative p-0">
+        @include('layouts.site.nav')
+        @yield('carousel')
+    </div>
+    {{-- NAV END --}}
+    @yield('search-box')
+
+    @yield('content')
+
+    @include('layouts.site.footer')
+
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary btn-primary-outline-0 btn-md-square back-to-top"><i
+            class="fa fa-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/site/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('assets/site/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/site/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/site/lib/lightbox/js/lightbox.min.js') }}"></script>
+
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('assets/site/js/main.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/site/nav.blade.php
@@ -0,0 +1,101 @@
+<!-- Navbar & Hero Start -->
+<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+    <a href="" class="navbar-brand p-0">
+        <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>{{ config('app.name') }}</h1>
+        <!-- <img src="img/logo.png" alt="Logo"> -->
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+            <a href="index.html" class="nav-item nav-link active">Home</a>
+
+            <!-- SMART CONSULTATIONS Dropdown -->
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">SMART CONSULTATIONS</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">AI, DRONES, GENETICS AND GAMING</a>
+                    <a href="#" class="dropdown-item">SMART ENGINEERING</a>
+                    <a href="#" class="dropdown-item">SMART CITIES AND HOMES</a>
+                </div>
+            </div>
+
+            <!-- GRADUATE SCHOOL FOR BUSINESS & ENTREPRENEURSHIP -->
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">GRADUATE SCHOOL</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">MBA/DBA/DM</a>
+                    <a href="#" class="dropdown-item">PROFESSIONAL DIPLOMAS</a>
+                    <a href="#" class="dropdown-item">PROFESSIONAL CERTIFICATION PROGRAMS</a>
+                </div>
+            </div>
+
+            <!-- SMART TRAINING AND DEVELOPMENT -->
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">SMART TRAINING</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">TRADITIONAL AND E-TRAINING</a>
+                    <a href="#" class="dropdown-item">PROFESSIONAL COURSES</a>
+                    <a href="#" class="dropdown-item">CERTIFICATION PROGRAMS</a>
+                    <a href="#" class="dropdown-item">eTRAINING</a>
+                </div>
+            </div>
+
+            <!-- SMART INTERNATIONAL UNIVERSITIES HUB -->
+            <div class="nav-item dropdown d-none">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">SMART INTERNATIONAL HUB</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">UK</a>
+                    <a href="#" class="dropdown-item">US</a>
+                    <a href="#" class="dropdown-item">Australia</a>
+                </div>
+            </div>
+
+            <!-- SMART LOCAL UNIVERSITIES HUB -->
+            <a href="packages.html" class="nav-item nav-link">SMART LOCAL UNIVERSITIES</a>
+
+            <!-- ACADEMIC VENUES -->
+            <div class="nav-item dropdown d-none">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">ACADEMIC VENUES</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">UNIVERSITY CAMPUSES</a>
+                    <a href="#" class="dropdown-item">AUDITORIUM</a>
+                    <a href="#" class="dropdown-item">COMMERCIAL SPACES</a>
+                    <a href="#" class="dropdown-item">CAFETERIAS</a>
+                    <a href="#" class="dropdown-item">OPEN LANDSCAPE</a>
+                    <a href="#" class="dropdown-item">CONFERENCE FACILITIES</a>
+                </div>
+            </div>
+
+            <!-- SMART INCUBATORS -->
+            <div class="nav-item dropdown d-none">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">SMART INCUBATORS</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">ENTREPRENEURSHIP</a>
+                    <a href="#" class="dropdown-item">INTERVENTIONS</a>
+                    <a href="#" class="dropdown-item">SMART SMALL PROJECTS SUPPORT</a>
+                    <a href="#" class="dropdown-item">SUPPORT CENTRE</a>
+                    <a href="#" class="dropdown-item">FACILITIES</a>
+                    <a href="#" class="dropdown-item">PRIZES</a>
+                    <a href="#" class="dropdown-item">FINANCE</a>
+                </div>
+            </div>
+
+            <!-- REMOTE FREELANCING ARENA -->
+            <div class="nav-item dropdown d-none">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">REMOTE FREELANCING ARENA</a>
+                <div class="dropdown-menu m-0">
+                    <a href="#" class="dropdown-item">REMOTE FREELANCING PLATFORMS</a>
+                    <a href="#" class="dropdown-item">SUPPORT</a>
+                    <a href="#" class="dropdown-item">TRAINING</a>
+                    <a href="#" class="dropdown-item">SPACES</a>
+                </div>
+            </div>
+
+            <a href="contact.html" class="nav-item nav-link">Contact</a>
+        </div>
+        <a href="" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Get Quote</a>
+    </div>
+</nav>
+<!-- Navbar & Hero End -->
