1f715d75e5181b5f63a89248715aa9d370208882
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-08-16 21:37:35 +0300

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             PageSeeder::class,
             TeamSeeder::class,
             UniverstySeeder::class,
+            TrainingProgramCategorySeeder::class
         ]);
 
         // \App\Models\User::factory()->create([
